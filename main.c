0d744a16207501dc3612cdbbc5f5090ee5cca2e0
Keisuke Saitou <kesaitou@c1r7s1.42tokyo.jp>
2025-11-11 06:12:32 +0900

    pipex-init

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kesaitou <kesaitou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 18:23:38 by kesaitou          #+#    #+#             */
-/*   Updated: 2025/11/10 21:14:54 by kesaitou         ###   ########.fr       */
+/*   Updated: 2025/11/11 03:11:38 by kesaitou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,94 +65,153 @@ char	*join_cmd(char *path, char *cmd)
 	return (joined);
 }
 
-int	exex_cmd(char *av, char **envp)
+int	exex_cmd(t_args args, int ind)
 {
 	char	**path;
 	char	**argv;
 	char	*full_path;
 	int		i;
 
-	argv = ft_split(av, ' ');
+	argv = ft_split(args.av[ind], ' ');
 	if (!argv)
 		return (1);
-	path = search_path(envp);
+	path = search_path(args.envp);
 	if (!path)
 		return (1);
 	i = 0;
 	while (path[i])
 	{
-		full_path = join_cmd(path[i], argv[0]);
+		full_path = join_cmd(path[i], argv[ind]);
 		if (!full_path)
 			return (1);
-		execve(full_path, argv, envp);
+		execve(full_path, argv, args.envp);
 		i++;
 	}
 	return (1);
 }
 
-int	fork_process(char *av, char **envp, int in_fd, int ou_fd)
+int	fork_process(t_args args)
 {
-	int	pid;
+	pid_t	pid;
+	int		p[2];
+	int		i;
+	int		prev_read;
+	pid_t	last_pid;
 
-	if (in_fd == -1)
-		return (1);
-	pid = fork();
-	if (pid < 0)
-		return (1);
-	else if (pid == 0)
+	i = 0;
+	prev_read = -1;
+	last_pid = -1;
+	while (i < args.ac - 2)
 	{
-		if (dup2(in_fd, STDIN_FILENO) == -1)
-			_exit(1);
-		close(in_fd);
-		exex_cmd(av, envp);
-		_exit(126);
+		if (i < args.ac - 3)
+			pipe(p);
+		pid = fork();
+		if (pid < 0)
+			return (1);
+		else if (pid == 0)
+		{
+			if (i == 0)
+			{
+				if (dup2(args.in_fd, STDIN_FILENO) == -1)
+					_exit(1);
+				close(args.in_fd);
+				exex_cmd(args, i);
+					_exit(126);
+			}
+			else
+			{
+				if (dup2(prev_read, STDIN_FILENO) == -1)
+					_exit(1);
+				close(args.in_fd);
+				exex_cmd(args, i);
+					_exit(126);
+			}
+			if (i < args.ac - 2)
+			{
+				if (dup2(p[1], STDOUT_FILENO) == -1)
+					_exit(1);
+			}
+			else
+			{
+				if (dup2(args.ou_fd, STDOUT_FILENO) == -1)
+					_exit(1);
+			}
+			if (prev_read != -1)
+				close(prev_read);
+			if (i < args.ac - 2)
+			{
+				close(p[0]);
+				close(p[1]);
+			}
+			close(args.in_fd);
+			close(args.ou_fd);
+			exex_cmd(args, i);
+			_exit(126);
+						
+		}
+		if (prev_read != -1)
+			close(prev_read);
+		if (i < args.ac - 2)
+		{
+			close(p[1]);
+			prev_read = p[0];
+		}
+		if (i == args.ac - 2)
+			last_pid = pid;
+		i++;
 	}
+	close(args.in_fd);
+	close(args.ou_fd);
+	if (prev_read != -1)
+		close(prev_read);
+	
+	int st, exitcode = 1;
+    for (int k = 0; k < args.ac - 2; k++) {
+        pid_t w = wait(&st);
+        if (w == -1) { perror("wait"); break; }
+        if (w == last_pid) {
+            if (WIFEXITED(st))      exitcode = WEXITSTATUS(st);
+            else if (WIFSIGNALED(st)) exitcode = 128 + WTERMSIG(st);
+        }
+    }
+    return exitcode;
 }
 
-int	child_process()
-{
-	
-	
-	
-	
-	
-	
-	
-}
 
-int	branch_process(int ac, char **av, char **envp)
-{
-	int	i;
-	int	in_fd;
-	int	pid;
-	int	ou_fd;
-	int	p[2];
 
-	if (!ft_strcmp(av[1], "here_doc"))
+int	branch_process(t_args args)
+{
+	if (!ft_strcmp(args.av[1], "here_doc"))
 		return (0);
 	else
 	{
-		in_fd = open(av[1], O_RDONLY);
-		ou_fd = open(av[ac - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-		i = 2;
-		while (i < ac - 2)
-		{
-			if (!(i == 2 || i == ac - 2))
-			{
-				if (pipe(p) == -1)
-					return (1);
-			}
-			fork_process(av[i], envp, in_fd, ou_fd);
-			
-		}
+		if (fork_process(args) == ERROR)
+			return (ERROR);
 	}
-	close(in_fd);
 	return (0);
+}
+
+int	init_args(t_args *args, int ac, char **av, char **envp)
+{
+	args->ac = ac;
+	args->av = av;
+	args->envp = envp;
+	args->in_fd = open(av[1], O_RDONLY);
+	if (args->in_fd == -1)
+		return (ERROR);
+	args->ou_fd = open(av[ac - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (args->ou_fd == -1)
+		return (ERROR);
+	return (SUCCESS);
 }
 
 int	main(int ac, char **av, char **envp)
 {
-	if (branch_process(ac, av, envp) == 1)
+	t_args	args;
+
+	if (init_args(&args, ac, av, envp) == ERROR)
+		return (ERROR);
+	if (branch_process(args) == 1)
 		return (1);
 }
 
@@ -166,7 +225,20 @@ int	main(int ac, char **av, char **envp)
 
 
 file cmd cmd cmd cmd file
-		[2] [3] [4] [5]
+[0]		[1] [2] [3] [4] [5]
+		[1] | [2] | [3] | [4] | [5]
+		1    2     3     4    5
+
+i = 1;
+while(i < ac - 2)
+
+fork()するタイミングとpipe()するタイミング
+forkはコマンドを別プロセスで実行するために必要
+pipeは入出力を制御するために必要
+pipeは最後以外のコマンドを実行するタイミングで必要
+
+
+
 ac = 7
 i = 2
 i < ac - 2
--- pipex.h
+++ pipex.h
@@ -6,20 +6,28 @@
 /*   By: kesaitou <kesaitou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 18:23:53 by kesaitou          #+#    #+#             */
-/*   Updated: 2025/11/10 19:10:34 by kesaitou         ###   ########.fr       */
+/*   Updated: 2025/11/11 01:22:21 by kesaitou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
-#define PIPEX_H
+# define PIPEX_H
 
-#include "libft/includes/ft_printf.h"
-#include "libft/includes/get_next_line.h"
-#include "libft/includes/libft.h"
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+# include "libft/includes/ft_printf.h"
+# include "libft/includes/get_next_line.h"
+# include "libft/includes/libft.h"
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 
+typedef struct s_args
+{
+	int		ac;
+	char	**av;
+	char	**envp;
+	int		in_fd;
+	int		ou_fd;
 
+}			t_args;
 
 #endif
